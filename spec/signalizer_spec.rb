7edc5182b21afd88e948a7f5166b012c947d1a9a
Unegbu Kingsley <kingsobino@gmail.com>
2025-03-18 16:22:00 +0100

    fix computed signal

--- spec/signalizer_spec.rb
+++ spec/signalizer_spec.rb
@@ -4,11 +4,18 @@ RSpec.describe RSignals::Signalizer do
   before do
     stub_const "Klass", Class.new
     Klass.class_eval { include RSignals::Signalizer }
-    Klass.class_eval { signalize sigma: 7, frer: 8 }
+    Klass.class_eval { signalize sigma: 7, alpha: -> { sigma + 2 } }
   end
 
   it "should respond to signal method" do
     instance = Klass.new
     expect(instance).to respond_to :sigma
+  end
+
+  it "should computed reactive signal" do
+    instance = Klass.new
+    expect(instance.alpha).to eq 9
+    instance.sigma 10
+    expect(instance.alpha).to eq 12
   end
 end
