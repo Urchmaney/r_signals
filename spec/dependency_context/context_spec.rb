d40f190b398b0c6bb081205de1d4ca1f6c1b2f34
Unegbu Kingsley <kingsobino@gmail.com>
2025-03-23 06:35:19 +0100

    add test for signal caching

--- spec/dependency_context/context_spec.rb
+++ spec/dependency_context/context_spec.rb
@@ -28,5 +28,25 @@ RSpec.describe RSignals::DependencyContext do
       signal.signal(-> { 3 })
       expect(signal.signal).to eql(3)
     end
+
+    it "should use cache value unless change in dependency" do
+      signal = RSignals::DependencyContext::Signal.new self, 3
+      expect(signal).to receive(:signal).and_call_original.exactly(3).times
+
+      second_signal = RSignals::DependencyContext::Signal.new self, -> { signal.signal + 3 }
+      second_signal.signal
+      second_signal.signal
+      second_signal.signal
+      second_signal.signal
+      expect(second_signal.signal).to eq(6)
+
+      signal.signal 4
+
+      second_signal.signal
+      second_signal.signal
+      second_signal.signal
+      second_signal.signal
+      expect(second_signal.signal).to eq(7)
+    end
   end
 end
